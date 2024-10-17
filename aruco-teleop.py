fix rotation bug in aruco-teleop.py

aruco-teleop.py
@@ -79,6 +79,7 @@ mc = MyCobot("/dev/ttyAMA0", 1000000)
 
 tracking = False
 marker_ids = []
+last_time = time.time()
 
 while True:
     ret, frame = cap.read()
@@ -132,7 +133,9 @@ while True:
 
     end_time = time.time()
     elapsed_time = end_time - start_time
-    text = f"time elapsed: {elapsed_time:.3f}s"
+    total_time = end_time - last_time
+    text = f"time elapsed: {elapsed_time:.3f}s  total time:{total_time:.3f}s"
+    last_time = end_time
 
     cv2.putText(frame, text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
@@ -157,28 +160,27 @@ while True:
                 start_tvec = t_target2world
                 start_rMat = R_target2world
                 start_coods = mc.get_coords()
+                print("start_coods", start_coods)
                 origin_angles = np.array(start_coods[3:6])
-                origin_rMat = euler_angles_to_rotation_matrix(start_coods[3], start_coods[4], start_coods[5])
+                origin_rMat = euler_angles_to_rotation_matrix(np.radians(start_coods[3]), np.radians(start_coods[4]), np.radians(start_coods[5]))
                 tracking = True
 
     if tracking:
         # go to the target position
-        print("go to target position")
         new_tvec = t_target2world
         move_tvec = new_tvec - start_tvec
         new_rMat = R_target2world
         delta_r_Mat = new_rMat @ np.linalg.inv(start_rMat)
         final_r_Mat = delta_r_Mat @ origin_rMat
         new_angles = rotation_matrix_to_euler_angles(final_r_Mat)
-        new_coods = [start_coods[0] + move_tvec[0] * 1000,
-                     start_coods[1] + move_tvec[1] * 1000,
-                     start_coods[2] + move_tvec[2] * 1000,
-                     start_coods[3] + new_angles[0],
-                     start_coods[4] + new_angles[1],
-                     start_coods[5] + new_angles[2]]
-        print("start_coods", start_coods)
+        new_coods = [start_coods[0] + round(move_tvec[0] * 1000, 1),
+                     start_coods[1] + round(move_tvec[1] * 1000, 1),
+                     start_coods[2] + round(move_tvec[2] * 1000, 1),
+                     round(new_angles[0], 2),
+                     round(new_angles[1], 2),
+                     round(new_angles[2], 2)]
         print("new coods", new_coods)
-        #mc.send_coords(new_coods, 20, 1)    
+        mc.send_coords(new_coods, 20, 1)    
         #time.sleep(0.1) # wait for finish
 
 cap.release()