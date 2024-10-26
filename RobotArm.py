bug fix in dh_matrix calculation

RobotArm.py
@@ -13,7 +13,7 @@ class RobotArm:
         return np.array([
             [np.cos(theta), -np.sin(theta), 0, a],
             [np.sin(theta)*np.cos(alpha), np.cos(theta)*np.cos(alpha), -np.sin(alpha), -d*np.sin(alpha)],
-            [np.sin(theta)*np.sin(alpha), -np.cos(theta)*np.sin(alpha), np.cos(alpha), d*np.cos(alpha)],
+            [np.sin(theta)*np.sin(alpha), np.cos(theta)*np.sin(alpha), np.cos(alpha), d*np.cos(alpha)],
             [0, 0, 0, 1]
         ])
 