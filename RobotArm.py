b1065893b6bc7165e72b18c55393c450a4c9da46
Keith Han <keithhans@gmail.com>
2024-10-26 21:31:47 +0800

    update d parameters

--- RobotArm.py
+++ RobotArm.py
@@ -74,7 +74,7 @@ class RobotArm:
 # Example usage
 if __name__ == "__main__":
     # Define D-H parameters
-    d = [0.13156, 0, 0, 0.06462, 0.07318, 0.0486]  # Link offsets
+    d = [0.13156, 0, 0, 0.0634, 0.07505, 0.0456]  # Link offsets
     a = [0, 0, -0.1104, -0.096, 0, 0]  # Link lengths
     alpha = [0, np.pi/2, 0, 0, np.pi/2, -np.pi/2]  # Link twists
     theta = [0, -np.pi/2, 0, -np.pi/2, np.pi/2, 0]  # Joint angles (initial values)
